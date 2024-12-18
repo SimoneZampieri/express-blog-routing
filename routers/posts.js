459270a467c8a98125a3dea272a6b4ac19b859c7
SimoneZampieri <simonezampieri106@gmail.com>
2024-12-18 12:25:01 +0100

    [update] routers set

---
+++ routers/posts.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.router();
+
+//index
+
+router.get('/', (req, res) =>{
+    res.send('Lista dei post')
+});
+
+//show
+
+router.get('/:id', (req, res) =>{
+    res.send(`Dettagli del post con id ${req.params.id}`
+});
+
+//store
+
+router.post('/', (req, res) =>{
+    res.send('Nuovo post')
+});
+
+//update
+
+router.put('/:id', (req, res) =>{
+    res.send(`Modifico l'elemento con id ${req.params.id}`)
+});
+
+//modify
+
+router.patch('/:id', (req, res) =>{
+    res.send(`Modifico parzialmente l'elemento con id ${req.params.id}`)
+});
+
+//destroy
+
+router.delete('/:id', (req, res) =>{
+    res.send(`Elimino l'elemento con id ${req.params.id}`)
+})
+
+
+module.exports = router;
